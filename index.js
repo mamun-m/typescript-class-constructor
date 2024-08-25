2219c7600a14ee8c0bc9c09f979927ad74764e70
Mohammad mamun <miam6829@gmail.com>
2024-08-25 21:45:03 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// var Names: string[];
+// var Names: Array<string>;
+// Names = ["mia", "mamun", "khan", "hasan"];
+// console.log(Names);
+// multiple types of arrays
+var MultipleTypes;
+MultipleTypes = ["mia", 1, true];
+console.log(MultipleTypes);
---
+++ index.ts
@@ -0,0 +1,12 @@
+// var Names: string[];
+// var Names: Array<string>;
+
+// Names = ["mia", "mamun", "khan", "hasan"];
+
+// console.log(Names);
+
+// multiple types of arrays
+
+var MultipleTypes: (string | number | boolean)[];
+MultipleTypes = ["mia", 1, true];
+console.log(MultipleTypes);
