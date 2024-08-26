a30b9362b02cc27be2715ea4301bb3b98ef20ec7
Mohammad mamun <miam6829@gmail.com>
2024-08-26 11:55:26 +0600

    first commit

--- index.js
+++ index.js
@@ -1,49 +1,15 @@
-// any data types
-// var anyName: any;
-// anyName = "mamun khan";
-// anyName = 6345564;
-// anyName = true;
-// anyName=[10,30]
-// console.log(anyName);
-// object type of data
-// let user1: { name: string; age: number };
-// var user1: object;
-// var User1: object[];
-// User1 = [
-//   { name: "mamun khan", age: 21 },
-//   { name: "burning", age: 22 },
-// ];
-// console.log(User1);
-// var User: object[];
-// User = [];
-// var User1: { Name: string; age: number };
-// User1 = { Name: "mia mamun khan", age: 31 };
-// User.push(User1);
-// var User2: { name: string; id: number };
-// User2 = { name: "hello", id: 45 };
-// User.push(User2);
-// console.log(User);
-// for (let key in User) {
-//   console.log(User[key]["Name"]);
+// class User {
+//   firstName: string;
+//   age: number;
+//   constructor(firstName: string, age: number) {
+//     this.firstName = firstName;
+//     this.age = age;
+//   }
+//   firstfunction(): void {
+//     console.log(`firstName :${this.firstName} , age :${this.age}`);
+//   }
 // }
-// custom types typescript
-// type User = { name: string; age: number };
-// var users: object[];
-// users = [];
-// let user1: User;
-// user1 = { name: "mia", age: 21 };
-// users.push(user1);
-// let user2: User;
-// user2 = { name: "mamun", age: 22 };
-// users.push(user2);
-// let user3: User;
-// user3 = { name: "burning", age: 23 };
-// users.push(user3);
-// console.log(users);
-// type firstType = "GET" | "POST";
-// var getpostion: firstType;
-// getpostion = "GET";
-// function requestHanlder(firstTyp: firstType): void {
-//   console.log(firstTyp);
-// }
-// requestHanlder("GET");
+// var use1 = new User("mohammad", 21);
+// use1.firstfunction();
+// let use2 = new User("Mamun", 26);
+// use2.firstfunction();
