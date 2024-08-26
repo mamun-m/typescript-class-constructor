48b3030b6c52357a707df4427446c6b2b3eb8757
Mohammad mamun <miam6829@gmail.com>
2024-08-26 09:45:40 +0600

    second commit

--- index.js
+++ index.js
@@ -1,23 +1,49 @@
-// tuple data types
-// let userId: [string, number];
-// userId = ["mamun", 21];
-// console.log(userId);
-// enum data types  // duplicate value is not allowed here //
-// enum firstRequest {
-//   readData = 1,
-//   writeData = 3,
-//   deleteData,
+// any data types
+// var anyName: any;
+// anyName = "mamun khan";
+// anyName = 6345564;
+// anyName = true;
+// anyName=[10,30]
+// console.log(anyName);
+// object type of data
+// let user1: { name: string; age: number };
+// var user1: object;
+// var User1: object[];
+// User1 = [
+//   { name: "mamun khan", age: 21 },
+//   { name: "burning", age: 22 },
+// ];
+// console.log(User1);
+// var User: object[];
+// User = [];
+// var User1: { Name: string; age: number };
+// User1 = { Name: "mia mamun khan", age: 31 };
+// User.push(User1);
+// var User2: { name: string; id: number };
+// User2 = { name: "hello", id: 45 };
+// User.push(User2);
+// console.log(User);
+// for (let key in User) {
+//   console.log(User[key]["Name"]);
 // }
-// console.log(firstRequest.readData);
-// enum secondRequest {
-//   textData = "full request",
-//   deleteData = "hello function",
+// custom types typescript
+// type User = { name: string; age: number };
+// var users: object[];
+// users = [];
+// let user1: User;
+// user1 = { name: "mia", age: 21 };
+// users.push(user1);
+// let user2: User;
+// user2 = { name: "mamun", age: 22 };
+// users.push(user2);
+// let user3: User;
+// user3 = { name: "burning", age: 23 };
+// users.push(user3);
+// console.log(users);
+// type firstType = "GET" | "POST";
+// var getpostion: firstType;
+// getpostion = "GET";
+// function requestHanlder(firstTyp: firstType): void {
+//   console.log(firstTyp);
 // }
-// console.log(secondRequest.deleteData);
-var thirdRequest;
-(function (thirdRequest) {
-    thirdRequest["firstName"] = "hello mamun";
-    thirdRequest["lastName"] = "burning boy abir";
-    thirdRequest[thirdRequest["id"] = 46546] = "id";
-})(thirdRequest || (thirdRequest = {}));
-console.log(thirdRequest.id);
+// requestHanlder("GET");
--- index.ts
+++ index.ts
@@ -1,27 +1,67 @@
-// tuple data types
+// any data types
 
-// let userId: [string, number];
-// userId = ["mamun", 21];
-// console.log(userId);
+// var anyName: any;
+// anyName = "mamun khan";
+// anyName = 6345564;
+// anyName = true;
+// anyName=[10,30]
+// console.log(anyName);
 
-// enum data types  // duplicate value is not allowed here //
+// object type of data
 
-// enum firstRequest {
-//   readData = 1,
-//   writeData = 3,
-//   deleteData,
+// let user1: { name: string; age: number };
+
+// var user1: object;
+// var User1: object[];
+
+// User1 = [
+//   { name: "mamun khan", age: 21 },
+//   { name: "burning", age: 22 },
+// ];
+// console.log(User1);
+
+// var User: object[];
+// User = [];
+
+// var User1: { Name: string; age: number };
+// User1 = { Name: "mia mamun khan", age: 31 };
+// User.push(User1);
+
+// var User2: { name: string; id: number };
+// User2 = { name: "hello", id: 45 };
+// User.push(User2);
+// console.log(User);
+// for (let key in User) {
+//   console.log(User[key]["Name"]);
 // }
-// console.log(firstRequest.readData);
 
-// enum secondRequest {
-//   textData = "full request",
-//   deleteData = "hello function",
+// custom types typescript
+
+// type User = { name: string; age: number };
+
+// var users: object[];
+// users = [];
+
+// let user1: User;
+// user1 = { name: "mia", age: 21 };
+// users.push(user1);
+
+// let user2: User;
+// user2 = { name: "mamun", age: 22 };
+// users.push(user2);
+
+// let user3: User;
+// user3 = { name: "burning", age: 23 };
+// users.push(user3);
+
+// console.log(users);
+// type firstType = "GET" | "POST";
+
+// var getpostion: firstType;
+
+// getpostion = "GET";
+
+// function requestHanlder(firstTyp: firstType): void {
+//   console.log(firstTyp);
 // }
-// console.log(secondRequest.deleteData);
-
-enum thirdRequest {
-  firstName = "hello mamun",
-  lastName = "burning boy abir",
-  id = 46546,
-}
-console.log(thirdRequest.id);
+// requestHanlder("GET");
